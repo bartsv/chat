edc87d708ee8a1575cbf9cfe8956d6de08e1654e
bartsv <b.bartsv@gmail.com>
2020-10-31 15:29:02 +0100

    Add files via upload

---
+++ client.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+    <div align="center"></div>
+
+    <form method="post">
+        <table>
+            <tr>
+                <td>
+                    <label>Enter Message</label>
+                    <input type="text" name="txtMessage">
+                    <input type="submit" name="btnSend" value="Send">
+                </td>
+            </tr>
+            <?php
+                $host = "127.0.0.1";
+                $port = "20205";
+
+                if (isset($_POST['btnSend'])) {
+                    $msg = $_REQUEST['txtMessage'];
+                    $sock = socket_create(AF_INET, SOCK_STREAM, 0);
+                    socket_connect($sock, $host, $port);
+
+                    socket_write($sock, $msg, strlen($msg));
+
+                    $reply = socket_read($sock, 1924);
+                    $reply = trim($reply);
+
+                    $reply = "Server says:\t".$reply;
+                }
+            ?>
+            <tr>
+                <td>
+                    <textarea rows="10" cols="30"><?php echo @$reply; ?></textarea>
+                </td>
+            </tr>
+        </table>
+    </form>
+
+</body>
+</html>
